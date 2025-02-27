a12b01a03078315c9a1c6517744b84cbcbdaa173
HadsonRamalho <hadsonramalho@gmail.com>
2025-02-27 18:39:34 -0300

    corrigindo tailwind; adicionando mais personagens

--- src/core/personagens.rs
+++ src/core/personagens.rs
@@ -95,6 +95,163 @@ pub async fn lista_personagens()
                 visao: 1,
                 limitecompra: 3
             }
+        },
+        Personagem{
+            nome: "Lucky Duke".to_string(),
+            descricao: "Toda vez que for “checar” (como para Barril, Dinamite, Prisão, etc), 
+            ele vira 2 cartas e escolhe a que melhor representa seus interesses. 
+            As cartas são descartadas logo após.".to_string(),
+            atributos: Atributos{
+                vida_atual: 4,
+                vida_maxima: 4,
+                efeitos: vec![Efeito::LuckyDukeChecagem],
+                distancia: 1,
+                visao: 1,
+                limitecompra: 2
+            }
+        },
+        Personagem{
+            nome: "Paul Regret".to_string(),
+            descricao: "Todos os outros jogadores o veem a um a mais de distância.".to_string(),
+            atributos: Atributos{
+                vida_atual: 3,
+                vida_maxima: 3,
+                efeitos: vec![Efeito::PaulRegretDistancia],
+                distancia: 2,
+                visao: 1,
+                limitecompra: 2
+            }
+        },
+        Personagem{
+            nome: "Pedro Ramirez".to_string(),
+            descricao: "Na fase de compras dele, ele pode comprar a primeira carta da 
+            pilha de descartes, que fica sempre virada para cima.".to_string(),
+            atributos: Atributos{
+                vida_atual: 4,
+                vida_maxima: 4,
+                efeitos: vec![Efeito::PedroRamirezCompra],
+                distancia: 1,
+                visao: 1,
+                limitecompra: 2
+            }
+        },
+        Personagem{
+            nome: "Rose Doolan".to_string(),
+            descricao: "Enxerga todos os outros jogadores a um a menos de distância.".to_string(),
+            atributos: Atributos{
+                vida_atual: 4,
+                vida_maxima: 4,
+                efeitos: vec![Efeito::RoseDoolanVisao],
+                distancia: 1,
+                visao: 2,
+                limitecompra: 2
+            }
+        },
+        Personagem{
+            nome: "Bart Cassidy".to_string(),
+            descricao: "Toda vez que ele perder um ponto de vida, ele compra uma carta.".to_string(),
+            atributos: Atributos{
+                vida_atual: 4,
+                vida_maxima: 4,
+                efeitos: vec![Efeito::BartCassidyCompra],
+                distancia: 1,
+                visao: 1,
+                limitecompra: 2
+            }
+        },
+        Personagem{
+            nome: "Sid Ketchum".to_string(),
+            descricao: "Descarta 2 cartas da mão para recuperar 1 ponto de vida.
+             Pode fazer quantas vezes quiser, porém, não pode ter mais pontos de 
+             vida do que o número de balas na lateral da carta de Poder.".to_string(),
+            atributos: Atributos{
+                vida_atual: 4,
+                vida_maxima: 4,
+                efeitos: vec![Efeito::SidKetchumDescarte],
+                distancia: 1,
+                visao: 1,
+                limitecompra: 2
+            }
+        },
+        Personagem{
+            nome: "Black Jack".to_string(),
+            descricao: "Na fase de compras, ele mostra a segunda carta comprada do bolo. 
+            Se ela for vermelha (de Copas ou Ouros), ele compra mais uma.".to_string(),
+            atributos: Atributos{
+                vida_atual: 4,
+                vida_maxima: 4,
+                efeitos: vec![Efeito::BlackJackCompra],
+                distancia: 1,
+                visao: 1,
+                limitecompra: 2
+            }
+        },
+        Personagem{
+            nome: "Slab The Killer".to_string(),
+            descricao: "Força seus oponentes a usarem 2 cartas de Esquiva para poderem 
+            desviar de 1 Bang dele.".to_string(),
+            atributos: Atributos{
+                vida_atual: 4,
+                vida_maxima: 4,
+                efeitos: vec![Efeito::SlabTheKillerBang],
+                distancia: 1,
+                visao: 1,
+                limitecompra: 2
+            }
+        },
+        Personagem{
+            nome: "Calamity Janet".to_string(),
+            descricao: "Usa as cartas “Bang!” e “Esquiva” como se fossem a mesma coisa.".to_string(),
+            atributos: Atributos{
+                vida_atual: 4,
+                vida_maxima: 4,
+                efeitos: vec![Efeito::CalamityJanetBangEsquiva],
+                distancia: 1,
+                visao: 1,
+                limitecompra: 2
+            }
+        },
+        Personagem{
+            nome: "Suzy Lafayette".to_string(),
+            descricao: " Se tiver usado todas as cartas da mão, ela compra uma nova 
+            da pilha de compra. Pode repetir quantas vezes for necessário/possível/desejável."
+            .to_string(),
+            atributos: Atributos{
+                vida_atual: 4,
+                vida_maxima: 4,
+                distancia: 1,
+                visao: 1,
+                limitecompra: 2,
+                efeitos: vec![Efeito::SuzyLafayetteCompra]
+            }   
+        },
+        Personagem{
+            nome: "El Gringo".to_string(),
+            descricao: "Toda vez que perder 1 ponto de vida por causa da carta de um jogador, 
+            ele rouba 1 carta da mão dele. Caso ele não tenha mais cartas na mão dele, azar. 
+            A Dinamite pertence a mesa, então se ela explodir nele, ele não pega cartas de ninguém."
+            .to_string(),
+            atributos: Atributos{
+                vida_atual: 4,
+                vida_maxima: 4,
+                distancia: 1,
+                visao: 1,
+                limitecompra: 2,
+                efeitos: vec![Efeito::ElGringoRoubo]
+            }
+        },
+        Personagem{
+            nome: "Vulture Sam".to_string(),
+            descricao: "Toda vez que um jogador for eliminado, Sam pegará todas as cartas
+             da mão e da mesa do falecido para si.".to_string(),
+            atributos: Atributos{
+                vida_atual: 4,
+                vida_maxima: 4,
+                distancia: 1,
+                visao: 1,
+                limitecompra: 2,
+                efeitos: vec![Efeito::VultureSamRoubo]
+            }
         }
     ];
 
